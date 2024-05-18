0174deb86c565ccf0584e861ea8e9bf014a6df1e
atarbabgei <atarbabgei@gmail.com>
2024-05-18 19:29:56 +0100

    Add Vicon DataStream SDK installation script with cleanup

--- mocap_vicon/scripts/install_vicon_datastream_sdk.sh
+++ mocap_vicon/scripts/install_vicon_datastream_sdk.sh
@@ -8,7 +8,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Define the URL and file names
-SDK_URL="https://github.com/atarbabgei/miscellaneous/raw/main/motion-capture/vicon/DataStreamSDK_1.12.0.zip"
+SDK_URL="https://github.com/atarbabgei/miscellaneous/raw/main/mocap_vicon/lib/DataStreamSDK_1.12.0.zip"
 SDK_ZIP="DataStreamSDK_1.12.0.zip"
 SDK_DIR="DataStreamSDK_1.12.0"
 LIB_DIR="/usr/lib"
@@ -32,9 +32,12 @@ else
     exit 1
 fi
 
-# Unzip the downloaded file
-if unzip "$SDK_ZIP"; then
-    echo "Unzipped $SDK_ZIP successfully."
+# Create a directory for the SDK files
+mkdir -p "$SDK_DIR"
+
+# Unzip the downloaded file into the SDK directory
+if unzip "$SDK_ZIP" -d "$SDK_DIR"; then
+    echo "Unzipped $SDK_ZIP successfully into $SDK_DIR."
 else
     echo "Failed to unzip $SDK_ZIP" 1>&2
     exit 1
@@ -42,15 +45,6 @@ fi
 
 # Remove the downloaded zip file
 rm "$SDK_ZIP"
-
-# Check if the unzipped directory exists
-if [[ -d "$SDK_DIR" ]]; then
-    # Change the directory to the unzipped folder
-    cd "$SDK_DIR" || { echo "Failed to change directory to $SDK_DIR" 1>&2; exit 1; }
-else
-    echo "Directory $SDK_DIR does not exist after unzipping" 1>&2
-    exit 1
-fi
 
 # Install Vicon DataStream SDK shared libraries
 echo "Installing shared libraries, please wait..."
@@ -77,6 +71,14 @@ if ldconfig; then
     echo "Library cache updated successfully."
 else
     echo "Failed to update library cache" 1>&2
+    exit 1
+fi
+
+# Remove the SDK directory
+if rm -rf "$SDK_DIR"; then
+    echo "Removed temp folder successfully."
+else
+    echo "Failed to remove $SDK_DIR" 1>&2
     exit 1
 fi
 
